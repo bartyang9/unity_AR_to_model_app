0908cb35c8efad9e6fdd282baa42d8ab0b1a7d9e
bartyang9 <yikuangy@gmail.com>
2018-05-01 02:05:43 -0400

    add visualization

--- Assets/testPC.cs
+++ Assets/testPC.cs
@@ -5,6 +5,8 @@ using System.Runtime.InteropServices;
 using UnityEngine;
 using UnityEditor;
 using Meta;
+[RequireComponent(typeof(LineRenderer))]
+
 //using pcl;
 
 public class testPC : MetaBehaviour
@@ -13,21 +15,30 @@ public class testPC : MetaBehaviour
     [DllImport("wrapper", EntryPoint = "detectplane")]
     public unsafe static extern int detectplane(float* points, int size, float* transInfo);
     [DllImport("wrapper", EntryPoint = "dataConverter")]
-    public unsafe static extern int dataConverter(float* points, int size, float* vir_pose, float* initial_guess, float* output_pose, bool isFirst);
+    public unsafe static extern float[] dataConverter(float* points, int size, float* initial_guess, bool isFirst);
 
     bool isFirst = true;
     bool pause = true;
     bool pause2 = true;
     int cnt = 0;
-    float[] real_obj = new float [16];
-
+    float[] real_obj = new float[7];
+    //LineRenderer line;
+    float pre_score = 0;
+    Vector3 prev_frame = new Vector3(10,10,10);
     void Update()
     {
         GameObject myObject = GameObject.Find("object");
+        GameObject myCam = GameObject.Find("myCamera");
+        GameObject my_real_object = GameObject.Find("realObject");
+        GameObject Line = GameObject.Find("Line");
+        GameObject rotLine = GameObject.Find("rotLine");
+
+        //var line = my_real_object.AddComponent<LineRenderer>();
+        //Transform my_real_object;
 
         if (pause == false)
         {
-            Debug.Log("start the app");
+            //Debug.Log("start the app");
             Time.timeScale = 1;
 
             //Debug.Log("start the main function");
@@ -35,9 +46,9 @@ public class testPC : MetaBehaviour
             
             cnt = cnt + 1;
             
-            if (cnt % 200 == 0)
+            if (cnt % 25 == 0)
             {
-                Debug.Log("enter the pointclouddata");
+                //Debug.Log("enter the pointclouddata");
                 PointCloudMetaData metadata = new PointCloudMetaData();
                 metaContext.Get<InteractionEngine>().GetCloudMetaData(ref metadata);
                 PointCloudData<PointXYZConfidence> pointCloudData = new PointCloudData<PointXYZConfidence>(metadata.maxSize);
@@ -60,8 +71,8 @@ public class testPC : MetaBehaviour
                 */
                 int length = pointCloudData.points.Length;
                 int size = pointCloudData.size;
-                Debug.Log("point_size=" + size);
-                Debug.Log("point_length=" + length);
+                //Debug.Log("point_size=" + size);
+                //Debug.Log("point_length=" + length);
 
                 float[] psource = new float[size * 3];
                 float[] transInfo = new float[7];
@@ -94,7 +105,7 @@ public class testPC : MetaBehaviour
                                 }
                                 if (num >= size * 3) break;
                             }
-                            Debug.Log("num:" + num);
+                            //Debug.Log("num:" + num);
                             pt = null;
                             //var len = (pt - psource);
                             //Debug.Log("length between pt and psource="+len);
@@ -104,45 +115,120 @@ public class testPC : MetaBehaviour
                             if (pause2 == false)
                             {
                                 Debug.Log("Am I here?");
-                                float[] vir_pose = new float[7];
                                 
-                                fixed (float* vir_pose_ = vir_pose)
+                                fixed (float* initial_guess_= real_obj)
                                 {
-                                    fixed (float* initial_guess_= real_obj)
-                                    {
-                                        
-                                        vir_pose[0] = myObject.transform.position.x;
-                                        vir_pose[1] = myObject.transform.position.y;
-                                        vir_pose[2] = myObject.transform.position.z;
-                                        vir_pose[3] = myObject.transform.rotation.w;
-                                        vir_pose[4] = myObject.transform.rotation.x;
-                                        vir_pose[5] = myObject.transform.rotation.y;
-                                        vir_pose[6] = myObject.transform.rotation.z;
-
-                                        float[] output_pose = new float[16];
-                                        fixed (float* output_pose_ = output_pose)
-                                        {
-                                            dataConverter(temp_p, num, vir_pose_, initial_guess_, output_pose_, isFirst);
-                                        }
-                                        for (int i = 0; i < 16; i++)
-                                        {
-                                            real_obj[i] = output_pose[i];
-                                        }
-
-                                        if (isFirst) isFirst = false;
-                                        
-                                    }
+                                    real_obj = dataConverter(temp_p, num, initial_guess_, isFirst);
+                                    //var str = "pose_output:";
+                                    //for (var i = 0; i < real_obj.Length; i++)
+                                    //{
+                                    //    str = str + real_obj[i];
+                                    //}
+                                    //Debug.Log("output_pose0:" + real_obj[0]);// + "," + real_obj[2] + "," + real_obj[3] + "," + real_obj[4]);// + "," + real_obj[5] + "," + real_obj[6] + "," + real_obj[7] + "," + real_obj[8] + "," + real_obj[9] + "," + real_obj[10] + "," + real_obj[11] + "," + real_obj[12] + "," + real_obj[13] + "," + real_obj[14] + "," + real_obj[15] + ".");
+                                    //Debug.Log("output_pose1:" + real_obj[1]);
+                                    //Debug.Log("output_pose2:" + real_obj[2]);
+                                    //Debug.Log("output_pose3:" + real_obj[3]);
+                                    //Debug.Log("output_pose4:" + real_obj[4]);
+                                    //Debug.Log("output_pose5:" + real_obj[5]);
+                                    //Debug.Log("output_pose6:" + real_obj[6]);
+                                    //Debug.Log("output_pose7:" + real_obj[7]);
+                                    //Debug.Log("output_pose8:" + real_obj[8]);
+                                    //Debug.Log("output_pose9:" + real_obj[9]);
+                                    //Debug.Log("output_pose10:" + real_obj[10]);
+                                    //Debug.Log("output_pose11:" + real_obj[11]);
+                                    //Debug.Log("output_pose:" + real_obj[12]);
+                                    //Debug.Log("output_pose:" + real_obj[13]);
+                                    //Debug.Log("output_pose:" + real_obj[14]);
+                                    //Debug.Log("output_pose:" + real_obj[15]);
+                                    if (isFirst) isFirst = false;
                                     
                                 }
+
+
+                                //===================== visualization ==============================
+                                //float[] vir_pose = new float[7];
+                                //vir_pose[0] = myObject.transform.position.x;
+                                //vir_pose[1] = myObject.transform.position.y;
+                                //vir_pose[2] = myObject.transform.position.z;
+                                //vir_pose[3] = myObject.transform.rotation.w;
+                                //vir_pose[4] = myObject.transform.rotation.x;
+                                //vir_pose[5] = myObject.transform.rotation.y;
+                                //vir_pose[6] = myObject.transform.rotation.z;
+                                if (real_obj[0] == -1) return;
+
+                                Quaternion rot_real = new Quaternion(real_obj[1], real_obj[2], real_obj[3], real_obj[0]);
+                                Vector3 trans_real = new Vector3(real_obj[4], real_obj[5], real_obj[6]);
+                                float score = real_obj[7];
+                                float distance_real_to_model = (prev_frame - trans_real).magnitude;
+                                bool inFOV = trans_real.z > 0.12 && trans_real.x <  trans_real.z && trans_real.x > - trans_real.z && trans_real.y <  0.6*trans_real.z && trans_real.y > -0.6 * trans_real.z;
+
+                                if (score < 0.001 && distance_real_to_model< 0.1 && inFOV)
+                                {
+                                    my_real_object.transform.rotation = myCam.transform.rotation;
+                                    my_real_object.transform.position = myCam.transform.position;
+                                
+                                    my_real_object.transform.Translate(trans_real, Space.Self);
+                                    my_real_object.transform.Rotate(rot_real.eulerAngles,Space.Self);
+
+                                    //line.sortingLayerName = "OnTop";
+                                    //line.sortingOrder = 5;
+                                    var line = Line.GetComponent<LineRenderer>();
+                                    line.sortingOrder = 1;
+                                    var rotline = rotLine.GetComponent<LineRenderer>();
+                                    rotline.sortingOrder = 1;
+                                    //line.material = new Material(Shader.Find("Sprites/Default"));
+                                    //line.sortingLayerName = "Foreground";
+                                    if (distance_real_to_model > 0.15)
+                                    {
+                                        line.material.color = Color.red;
+                                    }
+                                    else if (distance_real_to_model > 0.05)
+                                    {
+                                        line.material.color = Color.yellow;
+                                    }
+                                    else
+                                    {
+                                        line.material.color = Color.green;
+                                    }
+
+                                    Quaternion rot_diff = Quaternion.Inverse(my_real_object.transform.rotation) * myObject.transform.rotation;
+                                    Vector3 rot_axis = new Vector3(0,0,0);
+                                    float rot_angle =0;
+                                    rot_diff.ToAngleAxis(out rot_angle, out rot_axis);
+
+
+                                    var point1 = my_real_object.transform.position;
+                                    var point2 = myObject.transform.position;
+                                    point1.y += 0.05f;
+                                    point2.y += 0.05f;
+                                    line.positionCount = 2;
+                                    line.SetPosition(0, point1);
+                                    line.SetPosition(1, point2);
+                                    line.SetWidth(0.025f, 0.005f);
+                                    line.useWorldSpace = true;
+
+                                    var point2r = point1 + 0.05f * rot_angle * rot_axis;
+                                    rotline.positionCount = 2;
+                                    rotline.SetPosition(0, point1);
+                                    rotline.SetPosition(1, point2r);
+                                    rotline.SetWidth(0.025f, 0.005f);
+                                    rotline.useWorldSpace = true;
+
+                                    Debug.Log("the pre score is :" + pre_score);
+                                    Debug.Log("The curr score is:" + score);
+                                    
+                                }
+                                prev_frame = trans_real;
+                                pre_score = score;
                                 
                             }
                             else
                             { 
                                 detectplane(temp_p, num, transinfo);
-                                Vector3 trans_cam2obj = new Vector3(transinfo[0], transinfo[1], transinfo[2] + 0.05f);
+                                Vector3 trans_cam2obj = new Vector3(transinfo[0], transinfo[1], transinfo[2]);
                                 Quaternion rot_cam2obj = new Quaternion(transinfo[3], transinfo[4], transinfo[5], transinfo[6]);
-
-                                GameObject myCam = GameObject.Find("myCamera");
+                                
+                               
                                 //Vector3 trans_world2cam = myCam.transform.position;
                                 //Quaternion rot_world2cam = myCam.transform.rotation;// * tempQ;
                                 myObject.transform.rotation = myCam.transform.rotation;
@@ -168,6 +254,9 @@ public class testPC : MetaBehaviour
                                 //Debug.Log("xyz"+myObject.transform.position.x + myObject.transform.position.y + myObject.transform.position.z);
 
                             }
+
+
+
                         }
                     }
 
@@ -190,7 +279,7 @@ public class testPC : MetaBehaviour
                 pause = false;
             }
         }
-        if (Input.GetKeyDown(KeyCode.Q))
+        if (Input.GetKeyDown(KeyCode.U))
         {
             if (pause == true)
             {
@@ -212,7 +301,7 @@ public class testPC : MetaBehaviour
                 pause2 = false;
             }
         }
-        if (Input.GetKeyDown(KeyCode.W))
+        if (Input.GetKeyDown(KeyCode.I))
         {
             if (pause2 == true)
             {
---
+++ Assets/visual.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Put this script on a Camera
+public class visual : MonoBehaviour
+{
+
+    // Fill/drag these in from the editor
+
+    // Choose the Unlit/Color shader in the Material Settings
+    // You can change that color, to change the color of the connecting lines
+    public Material lineMat;
+    
+
+    bool pause = true;
+    bool pause2 = true;
+    // Connect all of the `points` to the `mainPoint`
+
+    void DrawConnectingLines()
+    {
+        if (pause == false && pause2 == false)
+        {
+            // Loop through each point to connect to the mainPoint
+            //foreach (GameObject point in points)
+            //{
+            GameObject mainPoint = GameObject.Find("object");
+            GameObject point = GameObject.Find("realObject");
+            Vector3 mainPointPos = mainPoint.transform.position;
+            Vector3 pointPos = point.transform.position;
+
+            GL.Begin(GL.LINES);
+            lineMat.SetPass(0);
+            GL.Color(new Color(lineMat.color.r, lineMat.color.g, lineMat.color.b, lineMat.color.a));
+            GL.Vertex3(mainPointPos.x, mainPointPos.y, mainPointPos.z);
+            GL.Vertex3(pointPos.x, pointPos.y, pointPos.z);
+            GL.End();
+            //}
+        }
+    }
+
+    // To show the lines in the game window whne it is running
+    void OnPostRender()
+    {
+        DrawConnectingLines();
+    }
+
+    // To show the lines in the editor
+    void OnDrawGizmos()
+    {
+        DrawConnectingLines();
+    }
+}
